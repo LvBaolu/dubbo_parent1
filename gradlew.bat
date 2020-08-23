99ea47e4f5fade40e7401fd87b39d655949a8f64
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2020-08-23 10:13:02 +0000

    Update product.php

--- gradlew.bat
+++ gradlew.bat
@@ -30,4 +30,5 @@ if "%DIRNAME%"=="" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-@rem Resolve any "." and ".
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set AP
