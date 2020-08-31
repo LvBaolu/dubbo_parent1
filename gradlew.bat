7102c728c5570b692f7aa7ab8873364f64e84b0b
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2020-08-31 22:08:21 +0000

    Nova versão

--- gradlew.bat
+++ gradlew.bat
@@ -46,4 +46,5 @@ if %ERRORLEVEL% equ 0 goto execute
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
-echo Please set the JAVA_HOME variable in your env
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installat
