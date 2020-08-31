422c30f1fe1931accf3aaded1a5e7bd3aa81a1ed
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2020-08-31 22:15:14 +0000

    fix interface

--- gradlew.bat
+++ gradlew.bat
@@ -47,4 +47,9 @@ echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installat
+echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_
