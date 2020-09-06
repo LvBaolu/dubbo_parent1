906c14d37d21cecc3abe0883ab807c74a260e9e9
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2020-09-06 08:03:28 +0000

    improved exception handling

--- gradlew.bat
+++ gradlew.bat
@@ -58,4 +58,6 @@ set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 if exist "%JAVA_EXE%" goto execute
 
 echo.
-echo ERROR: JAVA_H
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+ech
