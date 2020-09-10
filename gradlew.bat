6a24e683c886adfa7e09f697b1d2b8bbbaa95e76
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2020-09-10 20:16:16 +0000

    add codecovio and mocha json reporter

--- gradlew.bat
+++ gradlew.bat
@@ -72,4 +72,4 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 
 @rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_O
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname
