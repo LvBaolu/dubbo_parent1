82f3dd1ce34b8fccbe1226e56049979cb407644c
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2020-09-29 08:50:28 +0000

    Updating development poms to hotfix version to avoid merge conflicts

--- gradlew.bat
+++ gradlew.bat
@@ -83,4 +83,8 @@ rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instea
 rem the _cmd.exe /c_ return code!
 set EXIT_CODE=%ERRORLEVEL%
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
-if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_C
+if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
+exit /b %EXIT_CODE%
+
+:mainEnd
+if "%OS%"
