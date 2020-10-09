f545b3ee4c67e4a83274ad9c148472bc585271c8
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2020-10-09 13:29:55 +0000

    fixing names

--- src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
+++ src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
@@ -1,4 +1,6 @@
 package io.github.prismwork.emitrades;
 
 import com.google.common.collect.ImmutableSet;
-import dev.emi.emi.api.E
+import dev.emi.emi.api.EmiPlugin;
+import dev.emi.emi.api.EmiRegistry;
+import de
