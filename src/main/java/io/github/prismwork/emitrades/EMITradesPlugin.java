a0b9ade7973faa6178a0474de4bf23d56b0355fe
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2020-10-11 16:07:55 +0000

    dm thin: do not send discards to shared blocks

--- src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
+++ src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
@@ -3,4 +3,5 @@ package io.github.prismwork.emitrades;
 import com.google.common.collect.ImmutableSet;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
-import de
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.em
