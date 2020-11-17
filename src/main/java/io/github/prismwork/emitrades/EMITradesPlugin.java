e4db3dd2d236a667c5f551e427cde9f76eaf0e40
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2020-11-17 21:36:08 +0000

    Release 0.22.0

--- src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
+++ src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
@@ -44,4 +44,5 @@ public class EMITradesPlugin implements EmiPlugin {
     );
     public static final EmiRecipeCategory VILLAGER_TRADES
             = new EmiRecipeCategory(new Identifier("emitrades", "villager_trades"), EmiStack.of(Items.EMERALD));
-    public static EMITradesConfi
+    public static EMITradesConfig.Config CONFIG;
+    private static final File CONFIG_FILE 
