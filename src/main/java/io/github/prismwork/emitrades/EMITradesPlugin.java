d119d729e7a7b0ea7a15e207f6c2bd0ef2298e72
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2020-11-21 11:29:42 +0000

    xhci: Handle HS bulk/ctrl endpoints that don't NAK.

--- src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
+++ src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
@@ -45,4 +45,4 @@ public class EMITradesPlugin implements EmiPlugin {
     public static final EmiRecipeCategory VILLAGER_TRADES
             = new EmiRecipeCategory(new Identifier("emitrades", "villager_trades"), EmiStack.of(Items.EMERALD));
     public static EMITradesConfig.Config CONFIG;
-    private static final File CONFIG_FILE 
+    private static final File CONFIG_FILE = FabricLoader.getInstance().getConfigDir().resolve("emitrades
