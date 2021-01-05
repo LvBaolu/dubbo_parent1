a76bf39f183358b3a637d28fc49a9336122acc36
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2021-01-05 15:29:40 +0000

    ouch

--- src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
+++ src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
@@ -69,4 +69,4 @@ public class EMITradesPlugin implements EmiPlugin {
                 if (villager1 != null) {
                     villager1.setVillagerData(villager1.getVillagerData().withProfession(profession).withLevel(level + 1));
                 }
-   
+                for (TradeOffers.Factory offer : offers.get
