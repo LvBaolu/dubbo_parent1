5e6e58d3d91a1a8efb016db14204f33b9eb28a64
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2021-07-31 09:18:49 +0000

    cpuidle: Make it clear that governors cannot be modules

--- src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
+++ src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
@@ -119,4 +119,4 @@ public class EMITradesPlugin implements EmiPlugin {
                         }
                         int finalLevel = lvl;
                         genOffers.forEach(tradeOffer -> {
-                            registry.addRecipe(new VillagerTrade(new TradeProfile.DefaultImpl(WANDERING_TRADER_PLACEHOLDER, 
+                            registry.addRecipe(new VillagerTrade(new TradeProfile.DefaultImpl(WANDERING_TRADER_PLACEHOLDER, new FakeFactory(tradeOffer), finalLevel, wanderingTrader), wanderingTr
