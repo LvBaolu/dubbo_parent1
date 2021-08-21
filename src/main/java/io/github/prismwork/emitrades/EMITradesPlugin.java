cfd426518b62518166d8831960f70a933196016a
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2021-08-21 07:44:27 +0000

    Update index.tsx

--- src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
+++ src/main/java/io/github/prismwork/emitrades/EMITradesPlugin.java
@@ -142,4 +142,4 @@ public class EMITradesPlugin implements EmiPlugin {
     }
 
     private int compareOffers(@NotNull TradeOffer a, @NotNull TradeOffer b) {
-        int diff = Registries.ITEM.getRawId(a.g
+        int diff = Registries.ITEM.getRawId(a.getOriginalFirstBuyItem().getItem()) - Registries.ITEM.getRawId(b.get
