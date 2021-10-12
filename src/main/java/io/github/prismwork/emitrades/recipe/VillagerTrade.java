a1aea0571f377608ae953b3129f6a07b3cdfc206
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2021-10-12 15:20:01 +0000

    Développeur / Développeuse informatique

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -39,4 +39,5 @@ public class VillagerTrade implements EmiRecipe {
     private final int id;
     private final MutableText title;
 
-    publi
+    public VillagerTrade(TradeProfile profile, int id) {
+      
