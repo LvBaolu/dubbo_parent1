24f8b408288faad6f3fc43f50038ffefa319beeb
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2021-11-04 22:37:56 +0000

    fix travis build

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -60,4 +60,5 @@ public class VillagerTrade implements EmiRecipe {
             outputs.add(0, EmiStack.of(Items.EMERALD));
         } else if (offer instanceof TradeOffers.SellItemFactory factory) {
             inputs.add(0, EmiStack.of(Items.EMERALD, factory.price));
-  
+            inputs.add(1, EmiStack.EMPTY);
+            output
