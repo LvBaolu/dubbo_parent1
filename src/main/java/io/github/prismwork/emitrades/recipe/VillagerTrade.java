85562a9195e9f11263fbe586a0f8bdecf052f002
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2021-11-07 06:23:02 +0000

    config changed

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -62,4 +62,4 @@ public class VillagerTrade implements EmiRecipe {
             inputs.add(0, EmiStack.of(Items.EMERALD, factory.price));
             inputs.add(1, EmiStack.EMPTY);
             outputs.add(0, EmiStack.of(factory.sell, factory.count));
-        } else 
+        } else if (offer instanceof TradeOffers.SellSuspiciousStewFactory fact
