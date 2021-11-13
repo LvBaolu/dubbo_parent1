e5ec71a1e7f43b8dccab5ff2aa7ddeb312a15f14
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2021-11-13 18:51:36 +0000

    esp_scsi: Fix tag state corruption when autosensing.

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -65,4 +65,5 @@ public class VillagerTrade implements EmiRecipe {
         } else if (offer instanceof TradeOffers.SellSuspiciousStewFactory factory) {
             inputs.add(0, EmiStack.of(Items.EMERALD, 1));
             inputs.add(1, EmiStack.EMPTY);
-            ItemStack stack = new ItemStack(Items.SUSPICIOUS_STEW, 
+            ItemStack stack = new ItemStack(Items.SUSPICIOUS_STEW, 1);
+            SuspiciousStewItem.addEffectToStew(stack, factory
