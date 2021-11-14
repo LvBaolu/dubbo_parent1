445f1ef985b638c22b45887bf49af68aaab80fc9
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2021-11-14 06:17:01 +0000

    fix flake8 errors

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -66,4 +66,5 @@ public class VillagerTrade implements EmiRecipe {
             inputs.add(0, EmiStack.of(Items.EMERALD, 1));
             inputs.add(1, EmiStack.EMPTY);
             ItemStack stack = new ItemStack(Items.SUSPICIOUS_STEW, 1);
-            SuspiciousStewItem.addEffectToStew(stack, factory
+            SuspiciousStewItem.addEffectToStew(stack, factory.effect, factory.duration);
+            outputs.add
