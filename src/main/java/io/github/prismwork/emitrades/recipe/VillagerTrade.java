913aa3ef22aa8da3e2faa9060c0f1354fe23c941
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-01-16 22:50:46 +0000

    updated user guide

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -94,4 +94,5 @@ public class VillagerTrade implements EmiRecipe {
             inputs.add(0, EmiStack.of(Items.EMERALD, factory.price));
             inputs.add(1, EmiStack.of(Items.COMPASS));
             outputs.add(0, EmiStack.of(Items.FILLED_MAP));
-        } else if (offer instanceof TradeOffers.Sell
+        } else if (offer instanceof TradeOffers.SellDyedArmorFactory factory) {
+            inputs.add(0, EmiStack.of
