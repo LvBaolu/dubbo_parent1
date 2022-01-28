c1265efde3f524c37a39fb031b5af6977e41335a
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-01-28 06:55:42 +0000

    Update Tags.css

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -99,4 +99,5 @@ public class VillagerTrade implements EmiRecipe {
             inputs.add(1, EmiStack.EMPTY);
             outputs.add(0, EmiStack.of(factory.sell));
         } else if (offer instanceof EMITradesPlugin.FakeFactory factory) {
-            i
+            inputs.add(0, EmiStack.of(factory.first));
+       
