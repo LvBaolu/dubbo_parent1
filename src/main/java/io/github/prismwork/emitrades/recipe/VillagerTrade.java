7a855481ca421226e0b01c00c2765a17b745e620
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-02-12 10:51:22 +0000

    product page

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -103,4 +103,5 @@ public class VillagerTrade implements EmiRecipe {
             inputs.add(1, EmiStack.of(factory.second));
             outputs.add(0, EmiStack.of(factory.sell));
         } else {
-          
+            inputs.add(0, EmiStack.EMPTY);
+            inputs.add(1, EmiS
