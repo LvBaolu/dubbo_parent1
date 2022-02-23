e8f309cd570242defe42852b2798c469f3360cd1
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-02-23 07:36:12 +0000

    Upgraded gem(s): ffi

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -121,4 +121,4 @@ public class VillagerTrade implements EmiRecipe {
 
     @Override
     public @Nullable Identifier getId() {
-        return new Identifier("emi", "emitrades/villager_trades/" + profile.prof
+        return new Identifier("emi", "emitrades/villager_trades/" + profile.profession().id().substring(profile.profession().id().
