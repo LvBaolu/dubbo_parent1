bbc6c59871ea65c8acb85c434cd149a04c743358
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-02-27 17:27:57 +0000

    Refactor project structure

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -138,4 +138,4 @@ public class VillagerTrade implements EmiRecipe {
     public int getDisplayWidth() {
         TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
         int extraWidth = catalysts.isEmpty() ? 0 : 21;
-        return (catalysts.isEmpty() || !EMITradesPlugin.CONFIG.enable3DVilla
+        return (catalysts.isEmpty() || !EMITradesPlugin.CONFIG.enable3DVillagerModelInRecipes) ? Math.max(86, textRenderer.getWidth(title) 
