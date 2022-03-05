63b1dc63c2388bc6f26df2d88f474b7de504ef9b
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-03-05 09:56:57 +0000

    Replaced spaces with tabs

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -138,4 +138,5 @@ public class VillagerTrade implements EmiRecipe {
     public int getDisplayWidth() {
         TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
         int extraWidth = catalysts.isEmpty() ? 0 : 21;
-        return (catalysts.isEmpty() || !EMITradesPlugin.CONFIG.enable3DVillagerModelInRecipes) ? Math.max(86, textRenderer.getWidth(title) 
+        return (catalysts.isEmpty() || !EMITradesPlugin.CONFIG.enable3DVillagerModelInRecipes) ? Math.max(86, textRenderer.getWidth(title) + 2) :
+                Math.max(extraWidth + 85, extraWid
