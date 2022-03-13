9111886f2b60d9479cdd5991fda00e83c775a2ff
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-03-13 05:31:24 +0000

    doc: python bindings howto

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -150,4 +150,4 @@ public class VillagerTrade implements EmiRecipe {
     @Override
     public void addWidgets(WidgetHolder widgets) {
         TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
-        if (cataly
+        if (catalysts.isEmpty() || !EMITradesPlugin.CONFIG.enable3DVill
