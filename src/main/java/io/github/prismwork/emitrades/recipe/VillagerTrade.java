15b9b914f50a2fa6c7d73ce46af2d81a8c0e88b0
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-04-09 09:43:14 +0000

    docker build

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -161,4 +161,6 @@ public class VillagerTrade implements EmiRecipe {
         } else {
             SlotWidget villagerSlot = new SlotWidget(catalysts.get(0), 1, 6).drawBack(false);
             if (profile.villager() instanceof VillagerEntity villager) {
-                villagerSlot.appendTooltip(EmiPort.translatable("emi.emitrades.profession.lvl." + villager.getVillagerData().getLevel()).for
+                villagerSlot.appendTooltip(EmiPort.translatable("emi.emitrades.profession.lvl." + villager.getVillagerData().getLevel()).formatted(Formatting.YELLOW));
+            }
+            widg
