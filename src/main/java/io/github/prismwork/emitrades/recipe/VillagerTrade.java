45ccf204248e0509f83685bc5590065c8dea7a96
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-04-29 10:58:55 +0000

    Minors

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -177,4 +177,5 @@ public class VillagerTrade implements EmiRecipe {
     private void wrapOutput(WidgetHolder widgets, SlotWidget outputSlot) {
         if (profile.offer() instanceof TradeOffers.SellDyedArmorFactory) {
             outputSlot = outputSlot.appendTooltip(EmiPort.translatable("emi.emitrades.random_colored").formatted(Formatting.YELLOW));
-        } else if (profile.offer() instanceof TradeOffers.SellPotionHoldingItemFactory || profile.offer() instanceof TradeOffe
+        } else if (profile.offer() instanceof TradeOffers.SellPotionHoldingItemFactory || profile.offer() instanceof TradeOffers.SellSuspiciousStewFactory) {
+    
