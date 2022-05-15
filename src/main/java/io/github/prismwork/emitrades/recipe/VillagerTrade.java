5767f6604bab60cba1b79169aacdf3ebc8fed96e
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-05-15 22:04:25 +0000

    update report

--- src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
+++ src/main/java/io/github/prismwork/emitrades/recipe/VillagerTrade.java
@@ -188,3 +188,4 @@ public class VillagerTrade implements EmiRecipe {
                 outputSlot = outputSlot.appendTooltip(Text.translatable("emi.emitrades.enchantments.possible")
                         .formatted(Formatting.AQUA));
                 for (Enchantment enchantment : list) {
+                    outputSlot = outputSlot.appendTooltip
