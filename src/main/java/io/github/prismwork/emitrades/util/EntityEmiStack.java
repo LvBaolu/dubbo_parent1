6a51403be88f4084791fad082f742c33453474cf
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-05-22 15:13:20 +0000

    net: usb: Fix memory leak on Tx data path

--- src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
+++ src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
@@ -3,4 +3,5 @@ package io.github.prismwork.emitrades.util;
 import com.mojang.blaze3d.systems.RenderSystem;
 import dev.emi.emi.EmiPort;
 import dev.emi.emi.EmiUtil;
-import dev.emi.emi.api.stack.Em
+import dev.emi.emi.api.stack.EmiStack;
+import dev.emi.emi.screen.tooltip.Remain
