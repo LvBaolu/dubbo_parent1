d37c5f5a60a00c3d5144a5f9e01440035f6467ed
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-05-22 05:54:35 +0000

    Translated using Weblate (Afrikaans)

--- src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
+++ src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
@@ -1,4 +1,6 @@
 package io.github.prismwork.emitrades.util;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import dev.emi.emi.E
+import dev.emi.emi.EmiPort;
+import dev.emi.emi.EmiUtil;
+import dev.emi.emi.api.stack.Em
