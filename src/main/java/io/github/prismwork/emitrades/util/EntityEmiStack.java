596003087edc86265bb9c911679d21ee5cf703df
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-05-25 21:50:44 +0000

    Update arabic_lock.lua

--- src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
+++ src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
@@ -4,4 +4,5 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import dev.emi.emi.EmiPort;
 import dev.emi.emi.EmiUtil;
 import dev.emi.emi.api.stack.EmiStack;
-import dev.emi.emi.screen.tooltip.Remain
+import dev.emi.emi.screen.tooltip.RemainderTooltipComponent;
+import net.minecraft.client.M
