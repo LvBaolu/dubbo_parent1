4b087819c1f3001a0b95ac09677255095c55fd10
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-06-04 15:25:07 +0000

    add another solution

--- src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
+++ src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
@@ -11,4 +11,5 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
 import net.minecraft.client.render.DiffuseLighting;
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.ren
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.clien
