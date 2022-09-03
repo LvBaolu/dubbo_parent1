d462389ce2826ca5cc94420371260ba4644ca25c
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2022-09-03 16:53:08 +0000

    typo in last patch

--- src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
+++ src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
@@ -127,4 +127,6 @@ public class EntityEmiStack extends EmiStack {
 
     public static void renderEntity(int x, int y, double size, LivingEntity entity) {
         MinecraftClient client = MinecraftClient.getInstance();
-        Mouse mouse = client.mo
+        Mouse mouse = client.mouse;
+        float width = 1920;
+        float height = 1080
