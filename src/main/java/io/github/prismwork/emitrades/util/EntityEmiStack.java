192303fbef8cca39765eb42af99f1906e5117219
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2023-02-23 22:05:21 +0000

    Enable Windows builds on AppVeyor

--- src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
+++ src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
@@ -199,4 +199,5 @@ public class EntityEmiStack extends EmiStack {
         matrixStack.translate(x, y, 1050.0);
         matrixStack.scale(1.0F, 1.0F, -1.0F);
         RenderSystem.applyModelViewMatrix();
-        Matri
+        MatrixStack matrixStack2 = new MatrixStack();
+        matrix
