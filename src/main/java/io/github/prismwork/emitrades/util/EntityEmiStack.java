fe4ada581ac7d06116f6c3101b786b151cc8657b
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2023-02-25 10:31:49 +0000

    Raise version

--- src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
+++ src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
@@ -203,4 +203,5 @@ public class EntityEmiStack extends EmiStack {
         matrixStack2.translate(0.0, 0.0, 1000.0);
         matrixStack2.scale((float) size, (float) size, (float) size);
         Quaternionf quaternion = new Quaternionf().rotateZ(3.1415927F);
-        Quaternionf quaternion2 = new Quaternionf()
+        Quaternionf quaternion2 = new Quaternionf().rotateX(g * 20.0F * 0.017453292F);
+        quaternion.mul(qua
