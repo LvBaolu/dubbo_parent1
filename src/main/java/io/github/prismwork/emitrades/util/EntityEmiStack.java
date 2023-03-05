5d6faf2d9ca0f989043c7df2154e7296f074bffe
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2023-03-05 09:45:17 +0000

    Update nav.css

--- src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
+++ src/main/java/io/github/prismwork/emitrades/util/EntityEmiStack.java
@@ -218,4 +218,5 @@ public class EntityEmiStack extends EmiStack {
         VertexConsumerProvider.Immediate immediate = MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers();
         RenderSystem.runAsFancy(() -> entityRenderDispatcher.render(entity, 0.0, 0.0, 0.0, 0.0F, 1.0F, matrixStack2, immediate, 15728880));
         immediate.draw();
-        entityRenderD
+        entityRenderDispatcher.setRenderShadows(true);
+        entity.s
